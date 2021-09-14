91e926e6aa6c42bfb311ce8fd3effb81e4b41428
Ko.Yelie <ko.yelie@gmail.com>
2021-09-15 00:38:19 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { src, dest } = require('gulp');
+const gulpPug = require('gulp-pug');
+const gulpData = require('gulp-data');
+const imageSize = require('image-size');
+
+exports.default = function () {
+  return src(path.resolve(__dirname, 'src/**/*.pug'))
+    .pipe(
+      gulpData((file) => {
+        return {
+          imageSize: (src) => {
+            return imageSize(path.resolve(file.dirname, src));
+          },
+        };
+      })
+    )
+    .pipe(
+      gulpPug({
+        pretty: '  ',
+      })
+    )
+    .pipe(dest(path.resolve(__dirname, 'dist')));
+};

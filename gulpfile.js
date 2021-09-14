983b64b5ea62f1b733648eaba8ea2ca081d97318
Ko.Yelie <ko.yelie@gmail.com>
2021-09-15 03:11:19 +0900

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,23 @@ const gulpPug = require('gulp-pug');
 const gulpData = require('gulp-data');
 const imageSize = require('image-size');
 
+const sourcePath = path.resolve(__dirname, 'src');
+
 exports.default = function () {
-  return src(path.resolve(__dirname, 'src/**/*.pug'))
+  return src(path.resolve(sourcePath, '**/*.pug'))
     .pipe(
       gulpData((file) => {
+        // file: 処理中のPugファイルに関する情報
+        // file.dirname: 処理中のPugファイルのあるディレクトリパス
         return {
+          // imageSizeという名前の関数をPug内で使えるようにする
           imageSize: (src) => {
-            return imageSize(path.resolve(file.dirname, src));
+            // imgタグのsrc属性のパスを基にファイルパスを生成する
+            const filePath = src.startsWith('/')
+              ? path.resolve(sourcePath, src.slice(1)) // /から始まるルート相対パスの場合
+              : path.resolve(file.dirname, src); // 相対パスの場合
+            // ファイルパスに該当する画像のサイズをimage-sizeで取得する
+            return imageSize(filePath);
           },
         };
       })
